584b15c2a2170e603e9dff6b084871be8d948c5b
Abdul Rehman Khan <155987786+rehman874@users.noreply.github.com>
2024-01-20 12:35:33 +0500

    Add files via upload

---
+++ java assignment 2/app.js
@@ -0,0 +1,7 @@
+var username = prompt ('username')
+alert('Greeting'+'username')
+
+var length = prompt ('length of Rectangle')
+var width = prompt ('width of Rectangle')
+let area = length*width
+console.log('Area of the ractangle is'+ area);
